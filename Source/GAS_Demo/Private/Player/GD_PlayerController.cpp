94184d9ebecd190111f879a1f151e43346fa5e70
MingJu Sun <mingju.s@outlook.com>
2025-11-04 14:24:08 +0800

    通过重写PlayerControlle类中的SetupInputComponen函数，为游戏角色设置输入映射与动作。

---
+++ Source/GAS_Demo/Private/Player/GD_PlayerController.cpp
@@ -0,0 +1,67 @@
+﻿// MJSun
+
+
+#include "GAS_Demo/Public/Player/GD_PlayerController.h"
+#include "GAS_Demo/Public/Player/GD_PlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/Character.h"
+
+void AGD_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if (!IsValid(InputSubsystem)) return;
+
+	for (UInputMappingContext* Context: InputMappingContexts)
+	{
+		InputSubsystem->AddMappingContext(Context,0);
+	}
+
+	UEnhancedInputComponent* EnhancedInputComponent =Cast<UEnhancedInputComponent>(InputComponent);
+	if (!IsValid(EnhancedInputComponent)) return;
+
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+}
+
+void AGD_PlayerController::Jump()
+{
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->Jump();
+}
+
+void AGD_PlayerController::StopJumping()
+{
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->StopJumping();
+}
+
+void AGD_PlayerController::Move(const FInputActionValue& Value)
+{
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// Find which way is forward
+	const FRotator YawRotation(0.0f, GetControlRotation().Yaw, 0.0f);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
+}
+
+void AGD_PlayerController::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
---
+++ Source/GAS_Demo/Public/Player/GD_PlayerController.h
@@ -0,0 +1,38 @@
+﻿// MJSun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GD_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+
+UCLASS()
+class GAS_DEMO_API AGD_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+protected:
+	virtual void SetupInputComponent() override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gas")
+	TArray<TObjectPtr<UInputMappingContext>> InputMappingContexts;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Gas|Input")
+	TObjectPtr<UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gas|Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Gas|Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	void Jump();
+	void StopJumping();
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+};
