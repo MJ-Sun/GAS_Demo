7c2cbe68c3c85a80fa90cb4bda4fcb81a68a1fd4
MingJu Sun <mingju.s@outlook.com>
2025-11-05 12:57:22 +0800

    创建并初始化Enemy角色

---
+++ Source/GAS_Demo/Private/Characters/GD_EnemyCharacter.cpp
@@ -0,0 +1,36 @@
+﻿// MJSun
+
+
+#include "Characters/GD_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+AGD_EnemyCharacter::AGD_EnemyCharacter()
+{
+	
+	PrimaryActorTick.bCanEverTick = false;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* AGD_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void AGD_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent())) return;
+
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this,this);
+
+	if (!HasAuthority()) return;
+
+	GiveStartupAbilities();
+}
+
---
+++ Source/GAS_Demo/Public/Characters/GD_EnemyCharacter.h
@@ -0,0 +1,26 @@
+﻿// MJSun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GD_BaseCharacter.h"
+#include "GD_EnemyCharacter.generated.h"
+
+class UAbilitySystemComponent;
+
+UCLASS()
+class GAS_DEMO_API AGD_EnemyCharacter : public AGD_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGD_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)	
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
