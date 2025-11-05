649c74c5925c8c379b13b8d1e9d1f75e579f5dee
MingJu Sun <mingju.s@outlook.com>
2025-11-05 15:32:53 +0800

    编写碰撞检测测试

---
+++ Source/GAS_Demo/Public/AbilitySystem/Abilities/Player/GD_Primary.h
@@ -0,0 +1,31 @@
+﻿// MJSun
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/GD_GameplayAbility.h"
+#include "GD_Primary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_DEMO_API UGD_Primary : public UGD_GameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Gas|Abilities")
+	void HitBoxOverlapTest();
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxRadius = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxForwardOffset = 200.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxElevationOffset = 20.0f;
+};
