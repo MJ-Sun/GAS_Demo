c0355577d03c4b1c39cf6a659965c580572a5285
MingJu Sun <mingju.s@outlook.com>
2025-11-08 12:48:37 +0800

    创建监听属性变化的异步任务组件

---
+++ Source/GAS_Demo/Private/Tasks/GD_AttributeChangeTask.cpp
@@ -0,0 +1,39 @@
+﻿// MJSun
+
+
+#include "Tasks/GD_AttributeChangeTask.h"
+#include "AbilitySystemComponent.h"
+#include "DynamicMesh/DynamicMesh3.h"
+
+UGD_AttributeChangeTask* UGD_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAttribute Attribute)
+{
+	UGD_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<UGD_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ASC = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+
+	if (!IsValid(AbilitySystemComponent))
+	{
+		WaitForAttributeChangeTask->AddToRoot();
+		return nullptr;
+	}
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &UGD_AttributeChangeTask::AttributeChanged);
+
+	return WaitForAttributeChangeTask;
+}
+
+void UGD_AttributeChangeTask::EndTask()
+{
+	if (ASC.IsValid())
+	{
+		ASC->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UGD_AttributeChangeTask::AttributeChanged(const FOnAttributeChangeData& Data)
+{
+	OnAttributeChanged.Broadcast(Data.Attribute, Data.NewValue, Data.OldValue);
+}
